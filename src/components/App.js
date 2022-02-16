174d49ba72f1ec03d89726c2d41c7b2d8fb1ef07
GregT <taylor.gregory2@gmail.com>
2022-02-15 17:59:23 -0700

    Save

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,25 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handlePetClick() {
+    if(filters.type === "all"){
+    fetch('http://localhost:3001/pets')
+    .then(r => r.json())
+    .then(data => setPets(data))
+    }else{
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then(r => r.json())
+      .then(data => setPets(data))
+    }
+  }
+
+  function handleAdoptPet(petId){
+    console.log(petId)
+    const adoptedPet = pets.find((pet) => pet.id === petId)
+    console.log(adoptedPet)
+    adoptedPet.isAdopted = true
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +34,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={setFilters} onFindPetsClick={handlePetClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
           </div>
         </div>
       </div>
